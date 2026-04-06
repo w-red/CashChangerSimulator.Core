d95b7d3f6dba7a431d6d8d26f863fd7065094990
terashi <yterashi@gmail.com>
2026-04-06 16:05:08 +0900

    refactor(pos): introduce primary constructor to ClearOutputCommand

--- src/Device.PosForDotNet/Commands/ClearOutputCommand.cs
+++ src/Device.PosForDotNet/Commands/ClearOutputCommand.cs
@@ -4,28 +4,21 @@ using CashChangerSimulator.Device.Virtual;
 namespace CashChangerSimulator.Device.PosForDotNet.Commands;
 
 /// <summary>出金キャンセル操作をカプセル化するコマンド。</summary>
-public class ClearOutputCommand : IUposCommand
+public class ClearOutputCommand(DispenseController controller) : IUposCommand
 {
-    private readonly DispenseController controller;
+    private readonly DispenseController controller = controller;
 
-    /// <summary><see cref="ClearOutputCommand"/> クラスの新しいインスタンスを初期化します。</summary>
-    /// <param name="controller">出金コントローラー。</param>
-    public ClearOutputCommand(DispenseController controller)
-    {
-        this.controller = controller;
-    }
-
-    /// <summary>コマンドを実行します。</summary>
+    /// <inheritdoc/>
     public void Execute() => ExecuteAsync().GetAwaiter().GetResult();
 
-    /// <summary>コマンドを非同期で実行します。</summary>
+    /// <inheritdoc/>
     public Task ExecuteAsync()
     {
         controller.ClearOutput();
         return Task.CompletedTask;
     }
 
-    /// <summary>コマンドの実行条件を検証します。</summary>
+    /// <inheritdoc/>
     public void Verify(IUposMediator mediator)
     {
         // ClearOutput can usually be called even if disabled or not claimed?
